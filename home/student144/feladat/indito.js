3134e08229b1b6182b7ecb7adc9460791cc9eed6
nnarozsnik <nnarozsnik@gmail.com>
2025-12-05 14:04:22 +0100

    index, top pizza, kategoriak

--- home/student144/feladat/indito.js
+++ home/student144/feladat/indito.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const db = require('./db');
 
 // Statikus fájlok
 app.use(express.static(path.join(__dirname, 'public')));
@@ -9,13 +10,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'view'));
 app.set('view engine', 'ejs');
 
-// Fő oldal
-app.get('/', (req, res) => res.render('index'));
+app.use((req, res, next) => {
+  const kategoriaQuery = 'SELECT nev FROM kategoria ORDER BY nev';
+  const vegetarQuery = "SELECT nev FROM pizza WHERE vegetarianus = '1' ORDER BY nev";
+
+  db.query(kategoriaQuery, (err, kategorias) => {
+    if (err) return next(err);
+
+    db.query(vegetarQuery, (err2, vegetaris) => {
+      if (err2) return next(err2);
+
+     
+      res.locals.kategorias = kategorias;
+      res.locals.vegetaris = vegetaris;
+
+      next();
+    });
+  });
+});
 
 app.get('/contact', (req, res) => res.render('contact'));
-app.post('/contact', (req, res) => {
-  
+
+app.get('/', (req, res) => {
+  const pizzaQuery = 'SELECT * FROM pizza ORDER BY nev';
+  const popularQuery = `
+    SELECT p.nev, p.kategorianev, p.vegetarianus, k.ar, SUM(r.darab) AS total_ordered
+    FROM pizza p
+    JOIN rendeles r ON p.nev = r.pizzanev
+    JOIN kategoria k ON p.kategorianev = k.nev
+    GROUP BY p.nev, p.kategorianev, p.vegetarianus, k.ar
+    ORDER BY total_ordered DESC
+    LIMIT 10
+  `;
+
+  db.query(pizzaQuery, (err, pizzas) => {
+    if (err) throw err;
+
+    db.query(popularQuery, (err2, popularPizzas) => {
+      if (err2) throw err2;
+
+      res.render('index', { pizzas, popularPizzas });
+    });
+  });
 });
+
+app.get('/pizzak', (req, res) => {
+  const pizzaQuery = 'SELECT * FROM pizza ORDER BY nev';
+
+  db.query(pizzaQuery, (err, pizzas) => {
+    if (err) throw err;
+    res.render('pizzak', { 
+      pizzas
+    });
+  });
+});
+
 
 // app.get('/messages', isLoggedIn, (req, res) => {
  
